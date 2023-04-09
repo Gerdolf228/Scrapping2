09d79f5a213b0ec64dbb43a0b16f16d0f60c0071
Gerdolf228 <Westigor2@mail.ru>
2023-04-09 13:33:06 +0300

    Add user-agent

--- Scrapping2.py
+++ Scrapping2.py
@@ -2,3 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_data(url):
+    headers = {
+        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWevKit/537.36 (KHTML, Like Gecko) Chrome/86.0.4240.183 Mobile Safari/537.36"
+    }
